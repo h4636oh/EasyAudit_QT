02e622a207e37688f29770176f795f1556d9c89b
bhoomigundecha <bhoomigundecha@gmail.com>
2024-12-12 03:52:12 +0530

    done

--- scripts/audits/windows/18.9.20.1.6.ps1
+++ scripts/audits/windows/18.9.20.1.6.ps1
@@ -1,4 +1,3 @@
-#```powershell
 # PowerShell 7 Script to Audit the Group Policy Setting
 
 # Define the registry path and value name
@@ -21,7 +20,6 @@ try {
 }
 catch {
     Write-Warning "Audit Failed: Unable to retrieve the registry value. The setting might not be configured."
-    Write-Warning "Manual check required. Ensure the policy is set to 'Enabled' at the following path: `Computer Configuration\Policies\Administrative Templates\System\Internet Communication Management\Internet Communication settings\Turn off Internet download for Web publishing and online ordering wizards`"
+    Write-Warning "Manual check required. Ensure the policy is set to 'Enabled' at the following path: Computer Configuration\Policies\Administrative Templates\System\Internet Communication Management\Internet Communication settings\Turn off Internet download for Web publishing and online ordering wizards"
     exit 1
 }
-# ```
